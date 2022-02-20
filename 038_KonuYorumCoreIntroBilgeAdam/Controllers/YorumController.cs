d59840f22fc3f45e245c535fb4ccd9123242da0b
Ankbilgeadam@outlook.com <Ankbilgeadam@outlook.com>
2022-02-20 16:25:39 +0300

    v1

---
+++ 038_KonuYorumCoreIntroBilgeAdam/Controllers/YorumController.cs
@@ -0,0 +1,31 @@
+﻿using _038_KonuYorumCoreIntroBilgeAdam.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _038_KonuYorumCoreIntroBilgeAdam.Controllers
+{
+    public class YorumController : Controller
+    {
+        private BA_KonuYorumCoreContext _db = new BA_KonuYorumCoreContext();
+
+        public IActionResult Index()
+        {
+            // select * from Yorum order by Puan desc
+            //List<Yorum> yorumlar = _db.Yorum.OrderByDescending(yorum => yorum.Puan).ToList(); // OrderBy, OrderByDescending
+            // Bu şekilde ilişkili Konu kayıtları boş gelir.
+
+            // select * from Yorum inner join Konu on Yorum.KonuId = Konu.Id order by Puan desc, Yorumcu
+            List<Yorum> yorumlar = _db.Yorum.Include(yorum => yorum.Konu).OrderByDescending(yorum => yorum.Puan).ThenBy(yorum => yorum.Yorumcu).ToList(); // ThenBy, ThenByDescending
+
+            // Bir OrderBy kullanıldıktan sonra diğerlerinin hepsi ThenBy olmalıdır.
+
+            return View(yorumlar);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Yorum yorum = _db.Yorum.Include(yorum => yorum.Konu).SingleOrDefault(yorum => yorum.Id == id);
+            return View(yorum);
+        }
+    }
+}
--- 038_KonuYorumCoreIntroBilgeAdam/ReadMe.txt
+++ 038_KonuYorumCoreIntroBilgeAdam/ReadMe.txt
@@ -26,3 +26,6 @@ Proje geliştirme aşamaları:
 1.3) Create Action -> HttpGet ve HttpPost action method'ları, Create View'ının oluşturulması (View scaffolding), Create View'ındaki form verilerinin Create Post action'ına model olarak gönderilmesi ve veritabanında yeni kayıt oluşturulması, ViewBag ile ViewData kullanımı
 1.4) Edit Action -> LINQ Find, SingleOrDefault, FirstOrDefault ve LastOrDefault methodları
 1.5) Delete Action -> LINQ Include methodu ve veritabanında ilişkili kayıtların yönetilmesi, TempData kullanımı
+2) Yorum Controller
+2.1) Index Action -> Yorum listesinin (model) context üzerinden Index View'ına gönderilmesi ve Index View'ının oluşturulması (View scaffolding), ~/View/Shared/_Layout.cshtml'de sayfanın yukarısındaki menüye Yorumlar link'inin eklenmesi, Konu'nun da Yorum'lara Include edilmesi, OrderBy ve ThenBy kullanımı
+2.2) Details Action -> Yorum detayının (model) context üzerinden Details View'ına gönderilmesi ve Details View'ının oluşturulması (View scaffolding), Konu'nun da Yorum'a Include edilmesi
