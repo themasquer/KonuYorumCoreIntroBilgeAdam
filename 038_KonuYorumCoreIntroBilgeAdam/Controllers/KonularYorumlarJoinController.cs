09de642b26af790843fcb7f3e0bad32ba349b25f
Ankbilgeadam@outlook.com <Ankbilgeadam@outlook.com>
2022-02-27 17:22:59 +0300

    v1

---
+++ 038_KonuYorumCoreIntroBilgeAdam/Controllers/KonularYorumlarJoinController.cs
@@ -0,0 +1,35 @@
+﻿using _038_KonuYorumCoreIntroBilgeAdam.DataAccess;
+using _038_KonuYorumCoreIntroBilgeAdam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _038_KonuYorumCoreIntroBilgeAdam.Controllers
+{
+    public class KonularYorumlarJoinController : Controller
+    {
+        private BA_KonuYorumCoreContext _db = new BA_KonuYorumCoreContext();
+
+        /*
+        select k.Baslik, k.Aciklama, y.Icerik, y.Yorumcu, y.Puan 
+        from Konu k inner join Yorum y
+        on k.Id = y.KonuId
+        */
+        public IActionResult Index()
+        {
+            IQueryable<Konu> konuQuery = _db.Konu.AsQueryable(); // select * from Konu
+            IQueryable<Yorum> yorumQuery = _db.Yorum.AsQueryable(); // select * from Yorum
+            var joinQuery = from kq in konuQuery
+                            join yq in yorumQuery
+                            on kq.Id equals yq.KonuId
+                            select new KonuYorumInnerJoinModel()
+                            {
+                                Baslik = kq.Baslik,
+                                Aciklama = kq.Aciklama,
+                                Icerik = yq.Icerik,
+                                Yorumcu = yq.Yorumcu,
+                                Puan = yq.Puan
+                            };
+            var model = joinQuery.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ 038_KonuYorumCoreIntroBilgeAdam/Models/KonuYorumInnerJoinModel.cs
@@ -0,0 +1,19 @@
+﻿namespace _038_KonuYorumCoreIntroBilgeAdam.Models
+{
+    public class KonuYorumInnerJoinModel // KonuYorumInnerJoinDTO, DTO: Data Transfer Object
+    {
+        #region Konu entity'sinden gelen özellikler
+        //public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        #endregion
+
+        #region Yorum entity'sinden gelen özellikler
+        //public int Id { get; set; }
+        public string Icerik { get; set; }
+        public string Yorumcu { get; set; }
+        public int? Puan { get; set; }
+        //public int KonuId { get; set; }
+        #endregion
+    }
+}
