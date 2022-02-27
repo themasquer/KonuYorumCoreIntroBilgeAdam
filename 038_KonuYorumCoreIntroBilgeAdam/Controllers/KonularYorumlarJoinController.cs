356f070f3a582a0473ef3d4072ad977b134b32b2
Ankbilgeadam@outlook.com <Ankbilgeadam@outlook.com>
2022-02-27 19:43:44 +0300

    v1

--- 038_KonuYorumCoreIntroBilgeAdam/Controllers/KonularYorumlarJoinController.cs
+++ 038_KonuYorumCoreIntroBilgeAdam/Controllers/KonularYorumlarJoinController.cs
@@ -9,24 +9,59 @@ namespace _038_KonuYorumCoreIntroBilgeAdam.Controllers
         private BA_KonuYorumCoreContext _db = new BA_KonuYorumCoreContext();
 
         /*
-        select k.Baslik, k.Aciklama, y.Icerik, y.Yorumcu, y.Puan 
+        select k.Baslik, k.Aciklama, y.Icerik, y.Yorumcu, y.Puan, 
+        case when y.Puan < 3 then 'Kötü' when y.Puan = 3 then 'Orta' else 'İyi' end as PuanDurumu
         from Konu k inner join Yorum y
         on k.Id = y.KonuId
+        order by y.Puan desc, y.Yorumcu
         */
-        public IActionResult Index()
+        public IActionResult InnerJoin()
         {
             IQueryable<Konu> konuQuery = _db.Konu.AsQueryable(); // select * from Konu
             IQueryable<Yorum> yorumQuery = _db.Yorum.AsQueryable(); // select * from Yorum
-            var joinQuery = from kq in konuQuery
-                            join yq in yorumQuery
-                            on kq.Id equals yq.KonuId
+            var joinQuery = from konu in konuQuery
+                            join yorum in yorumQuery
+                            on konu.Id equals yorum.KonuId
+                            //where konu.Id == 5
+                            orderby yorum.Puan descending, yorum.Yorumcu
                             select new KonuYorumInnerJoinModel()
                             {
-                                Baslik = kq.Baslik,
-                                Aciklama = kq.Aciklama,
-                                Icerik = yq.Icerik,
-                                Yorumcu = yq.Yorumcu,
-                                Puan = yq.Puan
+                                Baslik = konu.Baslik,
+                                Aciklama = konu.Aciklama,
+                                Icerik = yorum.Icerik,
+                                Yorumcu = yorum.Yorumcu,
+                                Puan = yorum.Puan,
+
+                                PuanDurumu = yorum.Puan < 3 ? "Kötü" : yorum.Puan == 3 ? "Orta" : "İyi"
+                            };
+            var model = joinQuery.ToList();
+            return View(model);
+        }
+
+        /*
+        select k.Baslik, k.Aciklama, y.Icerik, y.Yorumcu, y.Puan, 
+        case when y.Puan < 3 then 'Kötü' when y.Puan = 3 then 'Orta' else 'İyi' end as PuanDurumu
+        from Konu k left outer join Yorum y
+        on k.Id = y.KonuId
+        order by y.Puan desc, y.Yorumcu
+        */
+        public IActionResult LeftOuterJoin()
+        {
+            IQueryable<Konu> konuQuery = _db.Konu.AsQueryable();
+            IQueryable<Yorum> yorumQuery = _db.Yorum.AsQueryable();
+            var joinQuery = from konu in konuQuery
+                            join yorum in yorumQuery
+                            on konu.Id equals yorum.KonuId into konuYorumJoin
+                            from subKonuYorumJoin in konuYorumJoin.DefaultIfEmpty()
+                            //where konu.Id == 5
+                            orderby subKonuYorumJoin.Puan descending, subKonuYorumJoin.Yorumcu
+                            select new KonuYorumLeftOuterJoinModel()
+                            {
+                                Baslik = konu.Baslik,
+                                Aciklama = konu.Aciklama,
+                                Icerik = subKonuYorumJoin.Icerik,
+                                Yorumcu = subKonuYorumJoin.Yorumcu,
+                                Puan = subKonuYorumJoin.Puan
                             };
             var model = joinQuery.ToList();
             return View(model);
--- 038_KonuYorumCoreIntroBilgeAdam/Models/KonuYorumLeftOuterJoinModel.cs
+++ 038_KonuYorumCoreIntroBilgeAdam/Models/KonuYorumLeftOuterJoinModel.cs
@@ -1,23 +1,16 @@
 ﻿namespace _038_KonuYorumCoreIntroBilgeAdam.Models
 {
-    public class KonuYorumInnerJoinModel // KonuYorumInnerJoinDTO, DTO: Data Transfer Object
+    public class KonuYorumLeftOuterJoinModel // KonuYorumLeftOuterJoinDTO, DTO: Data Transfer Object
     {
         #region Konu entity'sinden gelen özellikler
-        //public int Id { get; set; }
         public string Baslik { get; set; }
         public string Aciklama { get; set; }
         #endregion
 
         #region Yorum entity'sinden gelen özellikler
-        //public int Id { get; set; }
         public string Icerik { get; set; }
         public string Yorumcu { get; set; }
         public int? Puan { get; set; }
-        //public int KonuId { get; set; }
-        #endregion
-
-        #region Sayfanın ihtiyacına göre oluşturulan özellikler
-        public string PuanDurumu { get; set; }
         #endregion
     }
 }
